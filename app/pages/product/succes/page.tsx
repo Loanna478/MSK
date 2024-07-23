fb3617460b79d560bca85c8104a8acf964b103b8
Loen48 <MaskeZenDash@proton.me>
2024-07-23 23:20:57 +0200

    chore: Update package-lock.json and package.json to include ldrs v1.0.2

--- app/pages/product/succes/page.tsx
+++ app/pages/product/succes/page.tsx
@@ -4,6 +4,7 @@ import Particles from "@/components/particles";
 import Navbar from "@/components/navbar";
 import { useState, useEffect } from "react";
 import Button from "@/components/button";
+import 'ldrs/quantum';
 
 export default function Home() {
     const [loading, setLoading] = useState(true);
@@ -19,10 +20,9 @@ export default function Home() {
 
     if (loading) {
         return (
-            
             <div className="flex items-center justify-center min-h-screen bg-slate-900">
                 <Particles className="absolute inset-0 pointer-events-none" quantity={100} />
-                <img src="https://cdn0.iconfinder.com/data/icons/basic-ui-42/24/REFRESH_1-512.png" alt="Loading..." className="animate-spin h-16 w-16" />
+                <l-quantum size="45" speed="1.75" color="black"></l-quantum>
             </div>
         );
     }
@@ -38,7 +38,7 @@ export default function Home() {
                 </div>
             </center>
 
-            <Button href="http://mshop.ovh/.ovh/">Back to Home</Button>
+            <Button href="http://mshop.ovh/">Back to Home</Button>
         </div>
     );
 }
