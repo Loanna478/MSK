060545119d47652245ad6f72540973d36fa4e93e
Loen48 <MaskeZenDash@proton.me>
2024-07-24 01:39:04 +0200

    chore: Update ldrs package to v1.0.2 and optimize loading screen for product success page

--- app/pages/product/succes/page.tsx
+++ app/pages/product/succes/page.tsx
@@ -4,10 +4,14 @@ import Particles from "@/components/particles";
 import Navbar from "@/components/navbar";
 import { useState, useEffect } from "react";
 import Button from "@/components/button";
-import 'ldrs'
+import { quantum } from 'ldrs'
+
+
+
 
 export default function Home() {
     const [loading, setLoading] = useState(true);
+    quantum.register()
 
     useEffect(() => {
         // Simule un chargement de 3 secondes
@@ -22,7 +26,11 @@ export default function Home() {
         return (
             <div className="flex items-center justify-center min-h-screen bg-slate-900">
                 {/* <Particles className="absolute inset-0 pointer-events-none" quantity={100} /> */}
-                <l-quantum size="45" speed="1.75" color="black"></l-quantum>
+                <l-quantum
+                size="45"
+                speed="1.75" 
+                color="black" 
+                ></l-quantum>
             </div>
         );
     }
