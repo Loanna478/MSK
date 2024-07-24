edca38c7de5ec659d41b2710bf09f769e89616b3
Loen48 <MaskeZenDash@proton.me>
2024-07-24 02:22:19 +0200

    refactor: Update QuantumLoader component to use larger size and stroke in QuantumLoader.tsx

--- components/QuantumLoader.tsx
+++ components/QuantumLoader.tsx
@@ -9,7 +9,7 @@ const QuantumLoader = () => {
     });
   }, []);
 
-  return <l-infinity size="55" stroke="4" stroke-length="0.15" bg-opacity="0.1" speed="1.3"  color="white" ></l-infinity>;
+  return <l-infinity size="80" stroke="4" stroke-length="0.15" bg-opacity="0.1" speed="1.3"  color="white" ></l-infinity>;
 
 };
 
