35562f8af41749d411c66c2d3b13bd0c7141a04b
Loen48 <MaskeZenDash@proton.me>
2024-07-24 02:20:31 +0200

    refactor: Update QuantumLoader component to use infinity loader instead of waveform loader

--- components/QuantumLoader.tsx
+++ components/QuantumLoader.tsx
@@ -4,12 +4,13 @@ import { useEffect } from "react";
 const QuantumLoader = () => {
   useEffect(() => {
     import('ldrs').then((ldrs) => {
-      const { waveform } = ldrs;
-      waveform.register();
+      const { infinity  } = ldrs;
+      infinity .register();
     });
   }, []);
 
-  return <l-waveform size="45" speed="1.75" color="white"></l-waveform>;
+  return <l-infinity size="55" stroke="4" stroke-length="0.15" bg-opacity="0.1" speed="1.3"  color="white" ></l-infinity>;
+
 };
 
 export default QuantumLoader;
